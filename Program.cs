6812875b926ec521e4228d53d1adb6826a91d908
Agustin Leiria <leiria.a97@gmail.com>
2023-02-07 18:13:12 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Escribe un programa que reciba tu nombre y lo escriba por consola.
+
+Console.WriteLine("¿Como te llamas?");
+string name = Console.ReadLine();
+Console.WriteLine("Hola " + name);
+
+
+// Escribe un programa que tome la hora y la escriba por pantalla.
+
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine(datetime);
